e44598c65ccdfe24096fbee401475ecc55460e29
BruhMomentos <k.nguyen2003@hotmail.com>
2022-03-07 15:58:13 -0800

    yessir

--- calculator.py
+++ calculator.py
@@ -2,5 +2,5 @@ print(" Welcome to the the worlds most advanced Calculator")
 num1 = input("Enter a Number: ")
 num2 = input("Enter a another Number: ")
 result = float(num1) + float(num2)
+
 print(result)
-fag
